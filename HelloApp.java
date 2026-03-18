33880e33f5fed4cd88316fa0d8345676c7ebe41f
paarthmand <pm7785@srmist.edu.in>
2026-03-18 12:56:34 +0530

    Coded UC2 to display-name to console

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,14 @@
-class HelloApp{
-void main(){
-System.out.println("HELLO WORLD");
-}
+public class HelloApp {
+    public static void main(String[] args) {
+
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
 }
